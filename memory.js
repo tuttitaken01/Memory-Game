0d904f3b2ce4b778c1ae7fd5cab8596d9d97ca7a
Aleksanrda <kryvkoalexandra05@gmail.com>
2023-01-23 12:29:44 +0000

    add some solutions for some errors

--- memory.js
+++ memory.js
@@ -8,6 +8,7 @@ function getRandIndex(arr) {
 }
 
 function newGame() {
+    console.log("Starting the game");
     const emojis = ["😎", "😎", "😤", "😤", "😴", "😴", "🙄", "🙄", "😇", "😇", "😈", "😈"];
     const randEmojis = [];
 
@@ -17,29 +18,41 @@ function newGame() {
         randEmojis.push(currEmoji[0]);
     }
 
-    const buttons = document.getElementByClassName("in-game");
+    console.log(randEmojis, "<-- new array with random emojis");
+
+    const buttons = document.getElementsByClassName("in-game");
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].disabled = false;
-        buttons[i].innerText = "";
+        buttons[i].innerText = "error?";
         buttons[i].setAttribute(
             "onclick",
             `toggleEmoji('${randEmojis[i]}', ${ i + 1 })`
         );
         
     }
+    const score = document.getElementsByClassName("score");
+
     score.innerText = "Score: 0";
+    console.log(score.innerText);
 }
 
 function toggleEmoji(emoji, index) {
-    const button = document.getElementById(`button${index}`);
-    const prevButton = document.getElementById(`button${clicked.index}`);
+    const button = document.getElementById(`but${index}`);
+    const prevButton = document.getElementById(`but${clicked.index}`);
 
-    if(clicked.emoji === 0) {
-        clicked.emoji = emoji;
-        clicked.index = index;
-    }
+    console.log("toggle emoji");
+    console.log(document.getElementById(`but${index}`));
+    console.log(index);
+    console.log(clicked.index);
+    console.log(document.getElementById(`but${clicked.index}`));
 
+    // if(clicked.emoji === 0) {
+    //     clicked.emoji = emoji;
+    //     clicked.index = index;
+    // }
+    console.log(counter);
     if(emoji === clicked.emoji && clicked.index !== index) {
+        console.log("emoji are the same");
         button.disabled = true;
         prevButton.disabled = true;
 
@@ -48,7 +61,7 @@ function toggleEmoji(emoji, index) {
 
         counter += 1;
         score.innerText =`Score: ${counter}`;
-
+        console.log(counter);
         if (counter === 6) {
             document.getElementById("winMsg").style.color = "red";
         }
